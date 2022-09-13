1ebea8adcf0e77f2af5db61094fab0ffa2fbd041
Gabriele Namie <64558021+Gabrielenamie@users.noreply.github.com>
2022-09-13 16:45:48 -0300

    Add files via upload

---
+++ pocSignin/pocSignin/pocSigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocSigninApp.swift
+//  pocSignin
+//
+//  Created by Gabriele Namie on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct pocSigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
